d9fa54b74bfc50c887d22f65551a809b76f30890
Simon Peter Campbell <storm20200@gmail.com>
2014-10-18 23:41:21 +0100

    Changed the way some parts of the ScreenManager work for efficiency.

--- Source/Milestone1/ScreenManager/ScreenManager.cpp
+++ Source/Milestone1/ScreenManager/ScreenManager.cpp
@@ -1,29 +1,73 @@
 #include "ScreenManager.h"
 
 
-BYTE* ScreenManager::getScreenPointer()
+#pragma region Constructors and destructor
+
+
+ScreenManager::ScreenManager (const int screenWidth, const int screenHeight)
+    
+    : m_screenWidth (screenWidth), m_screenHeight (screenHeight), m_screenSize ((unsigned int) screenWidth * (unsigned int) screenHeight)
 {
-    if (!m_screen)
+    // We don't want silly screen resolutions now do we?
+    if (screenWidth < 1 || screenHeight < 1)
     {
-        m_screen = HAPI->GetScreenPointer();
+        throw std::runtime_error ("Invalid screen resolution given in ScreenManager::ScreenManager()");
     }
 
-    return m_screen;
+    // Check if a valid screen pointer is available
+    if (!(m_screen = HAPI->GetScreenPointer()))
+    {
+        throw std::runtime_error ("Unable to obtain screen pointer from HAPI.");
+    }
+}
+
+#pragma endregion Constructors and destructor
+
+
+
+
+#pragma region Colouring functionality
+
+
+void ScreenManager::clearToBlack (const unsigned int blackLevel)
+{
+    // Pre-condition: Black level is from 0 to 255.
+    if (m_screen && blackLevel < 256)
+    {
+        std::memset (m_screen, blackLevel, m_screenSize * m_kColourSize);
+    }
 }
 
 
-void ScreenManager::clearScreen (const HAPI_TColour& colour)
-{
-    const unsigned int colourSize = 4;
-    auto screenByte = getScreenPointer();
-
-    if (screenByte)
+void ScreenManager::clearToColour (const HAPI_TColour& colour)
+{    
+    // Ensure we have a valid pointer to the screen
+    
+    if (m_screen)
     {
-        for (unsigned int i = 0; i < getScreenSize(); ++i)
+        // Don't call setPixel, instead implement with unnecessary error checking removed for efficiency.
+        for (unsigned int i = 0; i < m_screenSize; ++i)
         {
-            auto pixel = screenByte + i * colourSize;
+            // Find the correct memory address
+            auto pixel = m_screen + i * m_kColourSize;
 
-            std::memcpy (pixel, &colour, colourSize);
+            std::memcpy (pixel, &colour, m_kColourSize);
         }
     }
 }
+
+
+void ScreenManager::setPixel (const unsigned int pixelNumber, const HAPI_TColour& colour)
+{
+    // Pre-condition: The screen pointer must be valid and a valid pixel number has been given
+    if (m_screen && pixelNumber < m_screenSize)
+    {
+        // Find and set the correct pixel
+        auto pixel = m_screen + pixelNumber * m_kColourSize;
+
+        std::memcpy (pixel, &colour, m_kColourSize);
+    }
+}
+
+
+#pragma endregion Colouring functionality
--- Source/Milestone1/ScreenManager/ScreenManager.h
+++ Source/Milestone1/ScreenManager/ScreenManager.h
@@ -2,27 +2,52 @@
 #define SCREEN_MANAGER_INCLUDED
 
 
-#include <HAPI_lib.h>
+#include <HAPI_lib.h>   // Required for m_screen.
 
 
 
+
+/// <summary>
+/// ScreenManager provides basic functionality required for a more robust way of managing the rendering capabilities of the HAPI library.
+/// </summary>
 class ScreenManager final
 {
     public:
-        ScreenManager (unsigned int screenWidth = 0, unsigned int screenHeight = 0) 
-                      : m_screenWidth (screenWidth), m_screenHeight (screenHeight) {}
+        #pragma region Constructors and destructor
 
-        void clearScreen (const HAPI_TColour& colour);
+        /// <summary> Create a ScreenManager with valid resolution values. </summary>
+        ScreenManager (const int screenWidth = 1, const int screenHeight = 1);
+
+        ScreenManager (const ScreenManager& copy)               = default;
+        ScreenManager& operator= (const ScreenManager& copy)    = default;
+
+        ~ScreenManager() = default;
+
+        #pragma endregion Constructors and destructor
+
+        
+        #pragma region Colouring functionality
+
+        /// <summary> Clears the screen to a black level between 0 and 255, quicker than clearing to a colour. </summary>
+        void clearToBlack (const unsigned int blackLevel = 0);
+
+        /// <summary> Clears the entire screen to a single colour. </summary>
+        void clearToColour (const HAPI_TColour& colour);
+
+        /// <summary> Sets the colour of an individual pixel. </summary>
+        void setPixel (const unsigned int pixelNumber, const HAPI_TColour& colour);
+
+        #pragma endregion Colouring functionality
 
     private:
-        BYTE*               getScreenPointer();
-        inline unsigned int getScreenSize()     { return m_screenWidth * m_screenHeight; }
+        
+        const size_t m_kColourSize = sizeof (HAPI_TColour); //!< The correct size of the HAPI_TColour.
 
+        int                 m_screenWidth = 0;      //!< The pixel width of the screen.
+        int                 m_screenHeight = 0;     //!< The pixel height of the screen.
+        unsigned int        m_screenSize = 0;       //!< How many pixels in total exist on the screen.
 
-        unsigned int m_screenWidth = 0;
-        unsigned int m_screenHeight = 0;
-
-        BYTE* m_screen = nullptr;
+        BYTE*               m_screen = nullptr;     //!< A cached copy of the HAPI screen pointer.
 };
 
 #endif // SCREEN_MANAGER_INCLUDED
