9f80fcf9e50ea347d60e333532d5337a05f4303a
Simon Peter Campbell <storm20200@gmail.com>
2014-11-04 20:04:40 +0000

    Moved min and max functions to a separate header.

---
+++ Source/Maths/Utilities.h
@@ -0,0 +1,33 @@
+#if !defined MATHS_UTILITIES_INCLUDED
+#define MATHS_UTILITIES_INCLUDED
+
+
+/// <summary> Returns the minimum value, parameters are passed by value. </summary>
+template <typename T> T min (const T a, const T b)
+{
+    return a < b ? a : b;
+}
+
+
+/// <summary> Returns the maximum value, parameters are passed by value. </summary>
+template <typename T> T max (const T a, const T b)
+{
+    return a > b ? a : b;
+}
+
+
+/// <summary> Returns the minimum value, parameters are passed by reference. </summary>
+template <typename T> T minR (const T& a, const T& b)
+{
+    return a < b ? a : b;
+}
+
+
+/// <summary> Returns the maximum value, parameters are passed by reference. </summary>
+template <typename T> T maxR (const T& a, const T& b)
+{
+    return a > b ? a : b;
+}
+
+
+#endif // MATHS_UTILITIES_INCLUDED
