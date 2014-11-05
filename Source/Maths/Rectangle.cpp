2799bc1312cd882978e4c314371f2791bd19cdbf
Simon Peter Campbell <storm20200@gmail.com>
2014-11-05 14:16:32 +0000

    Implemented draft Rectangle class.

---
+++ Source/Maths/Rectangle.cpp
@@ -0,0 +1,187 @@
+#include "Rectangle.h"
+
+
+/// STL headers.
+#include <stdexcept>
+#include <string>
+
+
+// Engine headers.
+#include <Maths/Utility.h>
+
+
+#pragma region Constructors and destructor
+
+
+Rectangle::Rectangle (const int left, const int top, const int right, const int bottom)
+{
+    // Test each component to check if it's valid.
+    if (left > right || right < left || top > bottom || bottom < top)
+    {
+        throw std::runtime_error ("Rectangle::Rectangle(): Invalid parameters given (" +    std::to_string (left) + ", " + 
+                                                                                            std::to_string (top) + ", " +
+                                                                                            std::to_string (right) + ", " + 
+                                                                                            std::to_string (bottom) + ")."  );
+    }
+
+    m_left = left;
+    m_top = top;
+    m_right = right;
+    m_bottom = bottom;
+}
+
+
+Rectangle::Rectangle (Rectangle&& move)
+{
+    *this = std::move (move);
+}
+
+
+Rectangle& Rectangle::operator= (Rectangle&& move)
+{
+    if (this != &move)
+    {
+        m_left = move.m_left;
+        m_top = move.m_top;
+        m_right = move.m_right;
+        m_bottom = move.m_bottom;
+    }
+
+    return *this;
+}
+
+
+#pragma endregion Constructors and destructor
+
+
+#pragma region Getters and setters
+
+
+void Rectangle::setLeft (const int left)
+{
+    if (left > m_right)
+    {
+        throw std::runtime_error ("Rectangle::setLeft(): Attempt to set the left co-ordinate with a value larger than m_right.");
+    }
+
+    m_left = left;
+}
+
+
+void Rectangle::setTop (const int top)
+{
+    if (top > m_bottom)
+    {
+        throw std::runtime_error ("Rectangle::setTop(): Attempt to set the top co-ordinate with a value larger than m_bottom.");
+    }
+
+    m_top = top;
+}
+
+
+void Rectangle::setRight (const int right)
+{
+    if (m_right < m_left)
+    {
+        throw std::runtime_error ("Rectangle::setRight(): Attempt to set the right co-ordinate with a value smaller than m_left.");
+    }
+
+    m_right = right;
+}
+
+
+void Rectangle::setBottom (const int bottom)
+{
+    if (bottom < m_top)
+    {
+        throw std::runtime_error ("Rectangle::setBottom(): Attempt to set the bottom co-ordinate with a value smaller than m_top.");
+    }
+
+    m_bottom = bottom;
+}
+
+
+#pragma endregion Getters and setters
+
+
+#pragma region Testing functionality
+
+
+bool Rectangle::operator== (const Rectangle& rhs) const
+{
+    return (m_left == rhs.m_left &&
+            m_top == rhs.m_top &&
+            m_right == rhs.m_right &&
+            m_bottom == rhs.m_bottom    );
+}
+
+
+bool Rectangle::operator!= (const Rectangle& rhs) const
+{
+    return (m_left != rhs.m_left ||
+            m_top != rhs.m_top ||
+            m_right != rhs.m_right ||
+            m_bottom != rhs.m_bottom    );
+}
+
+
+bool Rectangle::contains (const Rectangle& other) const
+{
+    return (m_left <= other.m_left &&
+            m_top <= other.m_top &&
+            m_right >= other.m_right &&
+            m_bottom >= other.m_bottom  );
+}
+
+
+bool Rectangle::intersects (const Rectangle& other) const
+{
+    return (m_left <= other.m_right &&      // A's left to B's right
+            m_top <= other.m_bottom &&      // A's top to B's bottom
+            m_right >= other.m_left &&      // A's right to B's left
+            m_bottom >= other.m_top     );  // A's bottom to B's top
+}
+
+
+#pragma endregion Testing functionality
+
+
+#pragma region Manipulation functionality
+
+
+Rectangle Rectangle::clipped (const Rectangle& clipTo) const
+{
+    // Clip each element.
+    const int   left    { max (m_left, clipTo.m_left) },
+                top     { max (m_top, clipTo.m_top) },
+                
+    // Ensure we maintain valid values.
+                right   { left > m_right ? left : min (m_right, clipTo.m_right) },
+                bottom  { top > m_bottom ? top : min (m_bottom, clipTo.m_bottom) };
+
+    return { left, top, right, bottom };
+}
+
+
+void Rectangle::clipTo (const Rectangle& other)
+{
+    // Clip each element.
+    m_left      = max (m_left, other.m_left);
+    m_top       = max (m_top, other.m_top);
+    
+    // Ensure we maintain valid values.
+    m_right     = m_left < m_right ? m_left : min (m_right, other.m_right);
+    m_bottom    = m_top < m_bottom ? m_top : min (m_bottom, other.m_bottom);
+}
+
+
+void Rectangle::translate (const int moveX, const int moveY)
+{
+    m_left += moveX;
+    m_top += moveY;
+    m_right += moveX;
+    m_bottom += moveY;
+}
+
+
+#pragma endregion Manipulation functionality
--- Source/Maths/Rectangle.h
+++ Source/Maths/Rectangle.h
@@ -12,7 +12,7 @@ class Rectangle final
         #pragma region Constructors and destructor
 
         /// <summary> Construct a rectangle with a value for each co-ordinate. </summary>
-        Rectangle (const int left, const int right, const int top, const int bottom);
+        Rectangle (const int left, const int top, const int roght, const int bottom);
 
         Rectangle()                                     = default;    
         Rectangle (const Rectangle& copy)               = default;
@@ -28,19 +28,13 @@ class Rectangle final
         #pragma region Getters and setters
 
         int getLeft() const     { return m_left; }
-        int getRight() const    { return m_right; }
         int getTop() const      { return m_top; }
+        int getRight() const    { return m_right; }
         int getBottom() const   { return m_bottom; }
 
-        /// <summary> Calculates and returns the width of the rectangle. </summary>
-        int calcWidth() const   { return m_right - m_left + 1; }
-        
-        /// <summary> Calculates and returns the height of the rectangle. </summary>
-        int calcHeight() const  { return m_bottom - m_top + 1; }
-
         void setLeft (const int left);
-        void setRight (const int right);
         void setTop (const int top);
+        void setRight (const int right);
         void setBottom (const int bottom);
 
         #pragma endregion
@@ -60,9 +54,29 @@ class Rectangle final
         #pragma endregion
 
 
+        #pragma region Calculation functionality
+
+        /// <summary> Calculates and returns the width of the rectangle. </summary>
+        int width() const       { return m_right - m_left + 1; }
+        
+        /// <summary> Calculates and returns the height of the rectangle. </summary>
+        int height() const      { return m_bottom - m_top + 1; }
+
+        /// <summary> Calculates the area of the rectangle based on the calculated width and height values. </summary>
+        int area() const        { return width() * height(); }
+
+        /// <summary> Calculates the perimeter of the rectangle based on the calculated width and height values. </summary>
+        int perimeter() const   { return width() * 2 + height() * 2; }
+
+        #pragma endregion
+
+
         #pragma region Manipulation functionality
 
-        /// <summary></summary>
+        /// <summary> Returns a clipped version of the current rectangle which is inside the given rectangle. </summary>
+        Rectangle clipped (const Rectangle& clipTo) const;
+
+        /// <summary> Will clip the current rectangle so that it is inside of the given rectangle. </summary>
         void clipTo (const Rectangle& other);
 
         /// <summary> Translates each co-ordinate by the given amount. </summary>
@@ -75,8 +89,8 @@ class Rectangle final
         #pragma region Member variables
 
         int m_left      { 0 },  //!< The left co-ordinate.
-            m_right     { 0 },  //!< The right co-ordinate.
             m_top       { 0 },  //!< The top co-ordinate.
+            m_right     { 0 },  //!< The right co-ordinate.
             m_bottom    { 0 };  //!< The bottom co-ordinate.
 
         #pragma endregion
