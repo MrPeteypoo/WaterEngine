cedcce030caa4f1c7d22cd1d5bd02898746f0380
Simon Peter Campbell <storm20200@gmail.com>
2014-12-06 23:34:55 +0000

    Mostly finished Renderer2DHAPI class.

---
+++ Source/Rendering/Renderer2DHAPI.cpp
@@ -0,0 +1,169 @@
+#include <Rendering/RendererHAPI.h>
+
+
+#pragma region Implmentation data
+
+
+// STL headers.
+#include <unordered_map>
+
+
+// Engine headers.
+#include <HAPI_lib.h>
+#include <Maths/Rectangle.h>
+#include <Maths/Vector2D.h>
+#include <Rendering/Texture.h>
+
+
+using Colour            = HAPI_TColour;
+
+
+const unsigned int  channel      = 255;
+const auto          sizeOfColour = sizeof (Colour);
+
+
+
+/// <summary> A POD structure with all working data for the HAPI renderer. </summary>
+struct Renderer2DHAPIImpl final
+{
+    BYTE*                               screen      { nullptr };    //!< A pointer to the memory address of the screen buffer.
+    Rectangle                           screenSpace { };            //!< A rectangle representing the screen space, used for clipping.
+    std::unordered_map<int, Texture>    textures    { };            //!< A container for all loaded texture data.
+};
+
+
+#pragma endregion
+
+
+
+#pragma region Constructors and destructor
+
+
+Renderer2DHAPI::Renderer2DHAPI()
+{
+    // Ensure we have our implementation data.
+    m_impl = new Renderer2DHAPIImpl();
+}
+
+
+Renderer2DHAPI::Renderer2DHAPI (Renderer2DHAPI&& move)
+{
+    // Just use the operator implementation.
+    *this = std::move (move);
+}
+
+
+Renderer2DHAPI& Renderer2DHAPI::operator= (Renderer2DHAPI&& move)
+{
+    if (this != &move)
+    {
+        // Ensure we don't have memory leaks.
+        if (m_impl)
+        {
+            delete m_impl;
+        }
+
+        // Take ownership of the movee implementation data.
+        m_impl = move.m_impl;
+
+        // Ensure we don't break the movee.
+        move.m_impl = new Renderer2DHAPIImpl();
+    }
+
+    return *this;
+}
+
+
+Renderer2DHAPI::~Renderer2DHAPI()
+{
+    // Clean up after ourselves.
+    if (m_impl)
+    {
+        delete m_impl;
+        m_impl = nullptr;
+    }
+}
+
+
+#pragma endregion Constructors and destructor
+
+
+
+#pragma region Initialisation
+
+
+void Renderer2DHAPI::initialise (const int screenWidth, const int screenHeight)
+{
+    // Pre-condition: Width and height are valid.
+    if (screenWidth <= 0 || screenHeight <= 0)
+    {
+        throw std::invalid_argument ("Renderer2DHAPI::initialise(): Invalid screen resolution given (" + 
+                                      std::to_string (screenWidth) + "x" + std::to_string (screenHeight) + ").");
+    }
+
+    // Initialise data.
+    m_impl->screen = HAPI->GetScreenPointer();
+    m_impl->screenSpace = { 0, 0, screenWidth - 1, screenHeight - 1 };
+
+    // Ensure the screen pointer is valid.
+    if (!m_impl->screen)
+    {
+        throw std::runtime_error ("Renderer2DHAPI::initialise(): Failed to obtain a pointer to the screenbuffer.");
+    }
+}
+
+
+void Renderer2DHAPI::clearTextureData()
+{
+    m_impl->textures.clear();
+}
+
+
+int Renderer2DHAPI::loadTexture (const std::string& fileLocation)
+{
+    return 0;
+}
+
+
+#pragma endregion Rendering
+
+
+
+#pragma region Rendering
+
+
+void Renderer2DHAPI::clearToBlack (const float blackLevel)
+{
+    const auto black = static_cast<BYTE> (channel * blackLevel);
+
+    // Use memset for efficiency.
+    std::memset (m_impl->screen, black, m_impl->screenSpace.area() * sizeOfColour);
+}
+
+
+void Renderer2DHAPI::clearToColour (const float red, const float green, const float blue, const float alpha)
+{
+    const auto      screenSize  =   m_impl->screenSpace.area();
+    const Colour    colour      {   static_cast<BYTE> (channel * red),
+                                    static_cast<BYTE> (channel * green), 
+                                    static_cast<BYTE> (channel * blue), 
+                                    static_cast<BYTE> (channel * alpha) };
+
+    // Loop through each pixel
+    for (unsigned int i = 0; i < screenSize; ++i)
+    {
+        // Find the correct memory address
+        auto pixel = m_impl->screen + i * sizeOfColour;
+
+        std::memcpy (pixel, &colour, sizeOfColour);
+    }
+}
+
+
+void Renderer2DHAPI::drawTexture (const Vector2D<int>& point, const int id, const int frame)
+{
+
+}
+
+
+#pragma endregion Rendering
---
+++ Source/Rendering/Renderer2DHAPI.h
@@ -0,0 +1,76 @@
+#if !defined RENDERER2D_HAPI_INCLUDED
+#define RENDERER2D_HAPI_INCLUDED
+
+
+// Engine headers.
+#include <Interfaces/IRenderer.h>
+
+
+// Forward declarations
+struct Renderer2DHAPIImpl;
+
+
+
+/// <summary>
+/// A 2D rendering engine which uses the HAPI library for all drawing requirements.
+/// </summary>
+class Renderer2DHAPI final : IRenderer2D
+{
+    public:
+
+        #pragma region Constructors & destructor
+
+        Renderer2DHAPI();
+        Renderer2DHAPI (Renderer2DHAPI&& move);
+        Renderer2DHAPI& operator= (Renderer2DHAPI&& move);
+
+        ~Renderer2DHAPI() final override;
+        
+        Renderer2DHAPI (const Renderer2DHAPI& copy) = delete;
+        Renderer2DHAPI& operator= (const Renderer2DHAPI&& copy) = delete;
+
+        #pragma endregion
+
+            
+        #pragma region Initialisation
+
+        /// <summary> Initialise all data and prepare for rendering. </summary>
+        void initialise (const int screenWidth, const int screenHeight) final override;
+
+        /// <summary> Causes all loaded texture data to be wiped. </summary>
+        void clearTextureData() final override;
+
+        /// <summary>
+        /// Loads a texture from local storage, ready for rendering. 
+        /// <returns> Returns the ID for the loaded texture. </returns>
+        /// </summary>
+        int loadTexture (const std::string& fileLocation) final override;
+        
+        #pragma endregion
+
+
+        #pragma region Rendering
+
+        /// <summary> Clears the screen to a black level between 0 and 1, quicker than clearing to a colour. </summary>
+        void clearToBlack (const float blackLevel = 0) final override;
+
+        /// <summary> Clears the entire screen to a single colour. Values should be between 0 and 1. </summary>
+        void clearToColour (const float red, const float green, const float blue, const float alpha = 1.f) final override;
+
+        /// <summary> 
+        /// Requests that a texture be drawn onto the screen at a particular point. 
+        /// </summary>
+        /// <param name="position"> The top-left point where the texture should render from. </param>
+        /// <param name="id"> The ID of the texture to render. </param>
+        /// <param name="frame"> Which frame to render from the texture. If no frames exist the entire texture will be drawn. </param>
+        void drawTexture (const Vector2D<int>& point, const int id, const int frame = 0) final override;
+
+        #pragma endregion
+
+    private:  
+
+        Renderer2DHAPIImpl* m_impl { nullptr };   //!< A pointer to the implementation data.
+};
+
+
+#endif // RENDERER_HAPI_INCLUDED
