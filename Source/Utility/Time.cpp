00054b726ca348b6b9e5d8695b5b034e6384d2b4
Simon Peter Campbell <storm20200@gmail.com>
2014-12-06 20:17:26 +0000

    Allowed for the specifying of any arithmetic type for the getCurrentTime() utility function.

--- Source/Utility/Time.cpp
+++ Source/Utility/Time.cpp
@@ -1,17 +1,6 @@
 #include "Time.h"
 
 
-time_t utility::getCurrentTime()
+time_t util::getCurrentTime()
 {
-    // Use chrono to convert from a time_point to time_t.
-    return  std::chrono::system_clock::to_time_t 
-            (
-                std::chrono::system_clock::now()
-            );
-}
-
-
-unsigned int utility::getCurrentTimeUInt()
-{
-    return (unsigned int) getCurrentTime();
 }
