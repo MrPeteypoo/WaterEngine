99a72c3c6cc293cd4b56e7ea8a24656bb6e9fb5f
Simon Peter Campbell <storm20200@gmail.com>
2014-12-06 22:46:22 +0000

    Updated IRenderer a little for extra functions.

--- Source/Interfaces/IRenderer.h
+++ Source/Interfaces/IRenderer.h
@@ -2,12 +2,14 @@
 #define INTERFACE_RENDERER_INCLUDED
 
 
+// STL headers.
+#include <string>
+
+
 // Forward declarations.
-struct Colour;
-template <typename T> class Vector2D;
+template <typename T> struct Vector2D;
 
 using Byte = unsigned char;
-namespace std { struct string; }
 
 
 
@@ -17,21 +19,26 @@ class IRenderer2D
     public:
         // Ensure destructor is virtual since this is an interface.
         virtual ~IRenderer2D() = 0;
+
     
         /// <summary> Initialise all data and prepare for rendering. </summary>
-        virtual bool initialise() = 0;
+        virtual bool initialise (const int screenWidth, const int screenHeight) = 0;
 
-        /// <summary> Clears the screen to a black level between 0 and 255, quicker than clearing to a colour. </summary>
-        virtual void clearToBlack (const Byte blackLevel = 0) = 0;        
-
-        /// <summary> Clears the entire screen to a single colour. </summary>
-        virtual void clearToColour (const Colour& colour) = 0;
+        /// <summary> Causes all loaded texture data to be deleted, invalidating all current keys. </summary>
+        virtual void cleanTextureData() = 0;
 
         /// <summary>
         /// Loads a texture from local storage, ready for rendering. 
         /// <returns> Returns the ID for the loaded texture. </returns>
         /// </summary>
         virtual int loadTexture (const std::string& fileLocation) = 0;
+
+
+        /// <summary> Clears the screen to a black level between 0 and 255, quicker than clearing to a colour. </summary>
+        virtual void clearToBlack (const Byte blackLevel = 0) = 0;        
+
+        /// <summary> Clears the entire screen to a single colour. </summary>
+        virtual void clearToColour (const Byte red, const Byte green, const Byte blue) = 0;
 
         /// <summary> 
         /// Requests that a texture be drawn onto the screen at a particular point. 
