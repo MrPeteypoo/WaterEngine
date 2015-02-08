286db292719ad15418ab4e4b53208fac007d2919
Simon Peter Campbell <storm20200@gmail.com>
2015-02-08 15:17:13 +0000

    The namespace for Water Token is wt.

--- Source/Game.h
+++ Source/Game.h
@@ -20,89 +20,91 @@ using Controller    = HAPI_TControllerData;
 using Keyboard      = HAPI_TKeyboardData;
 
 
-
-/// <summary>
-/// A basic encapsulation class for the game loop process, the handling of deltaTime and other main loop activities.
-/// </summary>
-class Game final
+// Game namespace
+namespace wt
 {
-    public: 
+    /// <summary>
+    /// A basic encapsulation class for the game loop process, the handling of deltaTime and other main loop activities.
+    /// </summary>
+    class Game final
+    {
+        public: 
         
-        #pragma region Constructors and destructor
+            #pragma region Constructors and destructor
 
-        // Use default construction and destruction.
-        Game()                              = default;
-        ~Game()                             = default;
+            // Use default construction and destruction.
+            Game()                              = default;
+            ~Game()                             = default;
 
-        // Delete all other constructors.
-        Game (const Game& copy)             = delete;
-        Game& operator= (const Game& copy)  = delete;
+            // Delete all other constructors.
+            Game (const Game& copy)             = delete;
+            Game& operator= (const Game& copy)  = delete;
 
-        Game (Game&& move)                  = delete;
-        Game& operator= (Game&& move)       = delete;
+            Game (Game&& move)                  = delete;
+            Game& operator= (Game&& move)       = delete;
 
-        #pragma endregion
+            #pragma endregion
 
-        /// <summary> Provides the entry point to the game. </summary>
-        void run();
+            /// <summary> Provides the entry point to the game. </summary>
+            void run();
         
 
-    private:
+        private:
 
-        #pragma region Engine functionality
+            #pragma region Engine functionality
 
-        /// <summary> 
-        /// Sets up HAPI and the various different systems, ready for use.
-        /// <returns> Whether the initialisation was successful or not. </returns>
-        /// </summary>
-        bool initialise();
+            /// <summary> 
+            /// Sets up HAPI and the various different systems, ready for use.
+            /// <returns> Whether the initialisation was successful or not. </returns>
+            /// </summary>
+            bool initialise();
 
-        /// <summary> Will update each deltaTime value, ready for usage by the different aspects of the game. </summary>
-        void updateDeltaTime();
+            /// <summary> Will update each deltaTime value, ready for usage by the different aspects of the game. </summary>
+            void updateDeltaTime();
 
-        /// <summary> The update function which handles all systems limited to 60FPS. </summary>
-        void updateCapped();
+            /// <summary> The update function which handles all systems limited to 60FPS. </summary>
+            void updateCapped();
 
-        /// <summary> The main update function which gets called each frame. </summary>
-        void updateMain();
+            /// <summary> The main update function which gets called each frame. </summary>
+            void updateMain();
 
-        /// <summary> Renders each object to the screen. </summary>
-        void renderAll();
+            /// <summary> Renders each object to the screen. </summary>
+            void renderAll();
 
-        #pragma endregion
+            #pragma endregion
 
 
-        #pragma region Member variables
+            #pragma region Member variables
 
-        int                                     m_screenWidth       { 1024 };        //!< The width of the screen in pixels.
-        int                                     m_screenHeight      { 1024 };        //!< The height of the screen in pixels.
+            int                                     m_screenWidth       { 1024 };        //!< The width of the screen in pixels.
+            int                                     m_screenHeight      { 1024 };        //!< The height of the screen in pixels.
 
-        DWORD                                   m_oldTime           { 0 };          //!< The previous time in milliseconds, used to calculate delta time.
-        DWORD                                   m_currentTime       { 0 };          //!< The current time in milliseconds, used to calculate delta time.
+            DWORD                                   m_oldTime           { 0 };          //!< The previous time in milliseconds, used to calculate delta time.
+            DWORD                                   m_currentTime       { 0 };          //!< The current time in milliseconds, used to calculate delta time.
 
-        float                                   m_deltaTime         { 0.f };        //!< The deltaTime calculated each frame.
-        float                                   m_sixtyFPSDeltaTime { 0.f };        //!< The deltaTime used to limit certain actions such as input to 60FPS.
+            float                                   m_deltaTime         { 0.f };        //!< The deltaTime calculated each frame.
+            float                                   m_sixtyFPSDeltaTime { 0.f };        //!< The deltaTime used to limit certain actions such as input to 60FPS.
 
-        bool                                    m_controllerOn      { false };      //!< Indicates whether a controller is plugged in and turned on.
+            bool                                    m_controllerOn      { false };      //!< Indicates whether a controller is plugged in and turned on.
 
-        Controller                              m_controller        {  };           //!< A cache of controller input, updated every 60 FPS.
-        Keyboard                                m_keyboard          {  };           //!< A cache of keyboard input, updated every 60 FPS.
+            Controller                              m_controller        {  };           //!< A cache of controller input, updated every 60 FPS.
+            Keyboard                                m_keyboard          {  };           //!< A cache of keyboard input, updated every 60 FPS.
 
-        Rectangle<float>                        m_centreZone        {  };           //!< The centre point of the screen.
+            Rectangle<float>                        m_centreZone        {  };           //!< The centre point of the screen.
 
-        std::vector<std::unique_ptr<IEntity>>   m_entities          {  };           //!< A vector containing every entity in the game.
+            std::vector<std::unique_ptr<IEntity>>   m_entities          {  };           //!< A vector containing every entity in the game.
 
-        std::shared_ptr<water::IRenderer>       m_renderer          { nullptr };    //!< The renderer used to draw everything onto the screen.
+            std::shared_ptr<water::IRenderer>       m_renderer          { nullptr };    //!< The renderer used to draw everything onto the screen.
 
-        #pragma endregion
+            #pragma endregion
 
-};
-
+    };
+}
 
 void HAPI_Main()
 {
-    // Create and run the game.
-    Game game { };
+    // Create and run water token!
+    wt::Game game { };
     game.run();
 }
 
