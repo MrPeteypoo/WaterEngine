7070ffd998f032771ae27a453d02168941c69306
Simon Peter Campbell <storm20200@gmail.com>
2015-02-20 04:58:41 +0000

    Allows lower case letters in mm::renderText() by forcing a conversion to lower case for given text.

--- Source/MagaMen/Misc/Utility.cpp
+++ Source/MagaMen/Misc/Utility.cpp
@@ -4,6 +4,7 @@
 // Engine headers.
 #include <Engine/Systems.hpp>
 #include <Engine/Misc/Vector2.hpp>
+#include <Engine/Utility/Misc.hpp>
 
 
 // Game namespace.
@@ -11,21 +12,22 @@ namespace mm
 {
     void renderText (const std::string& text, const water::TextureID charID, const water::TextureID numID, const Vector2<float>& position, const float distBetweenChar)
     {
-        // This is a very game-specific function where we know we only have character A-Z and numbers 0-9.
+        // This is a very game-specific function where we know we only have character a-z and numbers 0-9.
+        const auto& lowerText = util::toLower (text);
         auto textPosition = position;
-        for (auto i = 0U; i < text.size(); ++i)
+        for (auto i = 0U; i < lowerText.size(); ++i)
         {
             // Each character and number are individual frames of a texture.
             auto idToUse    = water::TextureID (0);
             auto frameToUse = -1;
-            char character  = text[i];
+            char character  = lowerText[i];
 
             // Check if the character is a number or letter.
-            if (character >= 'A' && character <= 'Z')
+            if (character >= 'a' && character <= 'z')
             {
-                // Calculate the frame by taking advantage of character order. We must ensure 'A' == 0 to get a correct frame offset.
+                // Calculate the frame by taking advantage of character order. We must ensure 'a' == 0 to get a correct frame offset.
                 idToUse     = charID;
-                frameToUse  = character - 'A';
+                frameToUse  = character - 'a';
             }
 
             else if (character >= '0' && character <= '9')
