c2d07dd937dc845cbd1e08c96a71484e691aed36
Simon Peter Campbell <storm20200@gmail.com>
2015-03-18 00:20:02 +0000

    Completed the window interfaces.

--- Source/Interfaces/IWindow.hpp
+++ Source/Interfaces/IWindow.hpp
@@ -9,6 +9,11 @@
 // Engine namespace.
 namespace water
 {
+    // Forward declarations.
+    using byte = unsigned char;
+    template <typename T> struct Vector2;
+
+
     /// <summary>
     /// An interface to every window management system in the engine. These are used to create graphical windows and manage
     /// window events such as controlling whether the application should close, etc.
@@ -25,92 +30,34 @@ namespace water
             // Getters and setters //
             /////////////////////////
 
-            /// <summary> Prepares a music track to be streamed. </summary>
-            /// <returns> Whether the music track was loaded successfully. </returns>
-            virtual bool loadMusic (const std::string& fileLocation) = 0;
+            /// <summary> Gets the width of the screen resolution. </summary>
+            /// <returns> The width in pixels. </returns>
+            virtual unsigned int getWidth() const = 0;
 
-            /// <summary> Loads a sound file into a sound buffer, this should be used for small sounds, not music. </summary>
-            /// <returns> The SoundID used to play the sound buffer. </returns>
-            virtual SoundID loadSound (const std::string& fileLocation) = 0;
+            /// <summary> Gets the height of the screen resolution. </summary>
+            /// <returns> The height in pixels. </returns>
+            virtual unsigned int getHeight() const = 0;
 
-            /// <summary> Deletes a loaded sound from the system. </summary>
-            /// <param name="sound"> The ID of the sound to delete. </param>
-            virtual void removeSound (const SoundID sound) = 0;
+            /// <summary> Gets the position of the window of the screen. </summary>
+            /// <returns> The position of the top-left point of the window in pixels. </returns>
+            virtual const Vector2<int>& getPosition() const = 0;
 
-            /// <summary> Causes all loaded sound data to be deleted, invaliding all current keys. </summary>
-            virtual void clearSoundData() = 0;
+            /// <summary> Sets the title of the window, this is displayed to the operating system. </summary>
+            /// <param name="title"> The text to display to the OS. </param>
+            virtual void setTitle (const std::string& title) = 0;
 
-
-            //////////////
-            // Playback //
-            //////////////
-
-            /// <summary> Plays the currently loaded music file. </summary>
-            /// <param name="volume"> The core volume of the music track. </param>
-            /// <param name="offset"> An offset in seconds can be provided to start the track at a particular point. </param>
-            /// <param name="loop"> Whether the track should be looped or not.</param>
-            virtual void playMusic (const float volume = 1.f, const float offset = 0.f, const bool loop = true) = 0;
-
-            /// <summary> Stops the music from playing entirely. </summary>
-            virtual void stopMusic() = 0;
-
-            /// <summary> Resumes the music track from the current point. </summary>
-            virtual void resumeMusic() = 0;
-
-            /// <summary> Pauses the music at its current position. </summary>
-            virtual void pauseMusic() = 0;
-
-            /// <summary> Plays the given sound with the desired parameters. </summary>
-            /// <param name="sound"> The loaded sound buffer to play. </param>
-            /// <param name="volume"> A normalised volume value from 0 to 1. </param>
-            /// <param name="offset"> An offset in seconds can be provided to start the sound at a particular point. </param>
-            /// <param name="loop"> Should the sound loop after finishing? </param>
-            /// <returns> The ID assigned to the newly playing sound. </returns>
-            virtual PlaybackID playSound (const SoundID sound, const float volume = 1.f, const float offset = 0.f, const bool loop = false) = 0;
-
-            /// <summary> Stops a particular sound from playing. Once you request it to stop you should never try to access the sound again. </summary>
-            virtual void stopSound (const PlaybackID sound) = 0;
-
-            /// <summary> Resumes a paused sound, this will start from where it left off. </summary>
-            virtual void resumeSound (const PlaybackID sound) = 0;
-
-            /// <summary> Pauses a particular sound, this will maintain its position. </summary>
-            virtual void pauseSound (const PlaybackID sound) = 0;
-
-            /// <summary> Causes all playing sounds to be stopped. </summary>
-            virtual void stopSounds() = 0;
-
-            /// <summary> Resumes all paused sounds. </summary>
-            virtual void resumeSounds() = 0;
-
-            /// <summary> Causes all playing sounds to be paused. </summary>
-            virtual void pauseSounds() = 0;
+            /// <summary> Sets the icon to be displayed by the operating system. </summary>
+            /// <param name="width"> The width of the icon in pixels. </param>
+            /// <param name="height"> The height of the icon in pixels. </param>
+            /// <param name="data"> A pointer to the image data. If the width or height values are invalid this could cause memory corruption. </param>
+            virtual void setIcon (const unsigned int width, const unsigned int height, const byte* const data) = 0;
 
 
-            //////////////////////
-            // Sound properties //
-            //////////////////////
-
-            /// <summary> Changes the effects mixer volume. </summary>
-            /// <param name="volume"> A normalised volume between 0 and 1. </param>
-            virtual void adjustEffectsMixer (const float volume) = 0;
-
-            /// <summary> Changes the music mixer volume. </summary>
-            /// <param name="volume"> A normalised volume between 0 and 1. </param>
-            virtual void adjustMusicMixer (const float volume) = 0;
-
-            /// <summary> Adjusts the properties of a background music. </summary>
-            /// <param name="volume"> A normalised volume value from 0 to 1. </param>
-            /// <param name="offset"> An offset in seconds for the track. </param>
-            /// <param name="loop"> Should the track loop after finishing? </param>
-            virtual void adjustMusicProperties (const float volume, const float offset, const bool loop) = 0;
-
-            /// <summary> Adjusts the properties of a playing sound. </summary>
-            /// <param name="sound"> The currently playing sound to modify. </param>
-            /// <param name="volume"> A normalised volume value from 0 to 1. </param>
-            /// <param name="offset"> An offset in seconds for the sound. </param>
-            /// <param name="loop"> Should the sound loop after finishing? </param>
-            virtual void adjustSoundProperties (const PlaybackID sound, const float volume, const float offset = 0.f, const bool loop = false) = 0;
+            /// <summary> Attempt to resize the window to the desired format. </summary>
+            /// <param name="width"> The desired width of the window. </param>
+            /// <param name="height"> The desired height of the window. </param>
+            /// <param name="fullscreen"> Whether fullscreen mode should be used. </param>
+            virtual void resize (const unsigned int width, const unsigned int height, const bool fullscreen) = 0;
     };
 }
 
--- Source/Systems/IEngineWindow.hpp
+++ Source/Systems/IEngineWindow.hpp
@@ -1,14 +1,42 @@
 #ifndef WATER_INTERFACE_WINDOW_ENGINE_INCLUDED
 #define WATER_INTERFACE_WINDOW_ENGINE_INCLUDED
 
+
+// Engine headers.
 #include <Interfaces/IWindow.hpp>
 
+
+// Engine namespace.
 namespace water
 {
+    /// <summary>
+    /// An interface to IWindow systems which allows critical functions to be performed by the Engine whilst being hidden from the user.
+    /// </summary>
     class IEngineWindow : public IWindow
     {
         public:
-            virtual ~IEngineWindow() {}
+
+            // Ensure destructor is virtual since this is an interface.
+            virtual ~IEngineWindow() override { }
+
+
+            ///////////////////////
+            // System management //
+            ///////////////////////
+
+            /// <summary> Initialise the system, opening the window with the specified settings. </summary>
+            /// <param name="width"> The desired width of the window in pixels. </param>
+            /// <param name="height"> The desired height of the window in pixels. </param>
+            /// <param name="fullscreen"> Whether the window should be fullscreened or not. </param>
+            /// <param name="title"> The title text to display to the operating system. </param>
+            virtual void initialise (const unsigned int width, const unsigned int height, const bool fullscreen, const std::string& title) = 0;
+
+            /// <summary> Update the system, ensuring all window events are handled and such. </summary>
+            /// <returns> Whether an error occurred, normally this should close the application. </returns>
+            virtual bool update() = 0;
+
+            /// <summary> Perform any necessary end-of-frame actions. </summary>
+            virtual void endFrame() = 0;
     };
 }
 
